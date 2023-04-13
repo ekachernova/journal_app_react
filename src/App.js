e13cee28e57ac248202abc9599d6ab6e1adf498c
Ekaterina Chernova <zvirbulyonok@gmail.com>
2023-04-13 16:10:42 +0200

    finish basic layout of the form

--- src/App.js
+++ src/App.js
@@ -5,8 +5,25 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <div  className ="EntryForm">
+      <h2 className = "EntryTitel">NEW ENTRY</h2>
+      <form id = "form" name ="form" type = "submit">
+        <label forHtml = "motto__input">
+          Motto
+        </label>
+        <input name= "motto__input" type = "text" id = "motto__input">
+        
+        </input>
+        <label forHtml = "notes__input">
+          Notes
+        </label>
+        <input name = "notes__input" type="text" id = "notes__input">
+        
+        </input>
+      </form>
+      </div>
     </div>
-  );
+    )
 }
 
 export default App;
